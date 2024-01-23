1ac5df42246121b4756b7227db1fea2274bda550
David Baranovski <david@dreamcubator.club>
2024-01-23 10:36:33 +0200

    last testing

--- src/extension.ts
+++ src/extension.ts
@@ -16,10 +16,30 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('helloworld.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from HelloWorld!');
+		vscode.window.showInformationMessage('Hello VS Code');
+	});
+	
+	let disposableTime = vscode.commands.registerCommand('helloworld.displayCurrentTime', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showWarningMessage(`Now is ${new Date().toLocaleTimeString()}`, 'Ok, thanks', 'Cancel').then((value) => {
+			if (value === 'Ok, thanks') {
+				vscode.window.showInformationMessage('You are welcome! 😁');
+			} else {
+				vscode.window.showInformationMessage('You are not welcome! 🗿');
+			}
+		});
+		
 	});
 
+	let disposableInput = vscode.commands.registerTextEditorCommand('helloworld.sampleCommand', (textEditor, edit) => {
+        // The code to be executed when the command is invoked
+        vscode.window.showInformationMessage('Sample Command Executed!');
+    });
+
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposableTime);
+	context.subscriptions.push(disposableInput);
 }
 
 // This method is called when your extension is deactivated
